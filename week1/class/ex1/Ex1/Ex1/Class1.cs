57d8df632a8bb0d003dadbbf90d34312124eca33
Ciprian <ciprian.strechie@gmail.com>
2020-03-06 15:55:31 +0200

    finalized EX 1

--- week1/class/ex1/Ex1/Ex1/Class1.cs
+++ week1/class/ex1/Ex1/Ex1/Class1.cs
@@ -1,8 +1,29 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Ex1
 {
     public class Class1
     {
+        public static void Main()
+        {
+            Stack<char> s = new Stack<char>();
+            int numberOfKeys = 3;
+            ConsoleKeyInfo chinput;
+            for (int i = 0; i < numberOfKeys; i++)
+            {
+                Console.WriteLine("Enter key:");
+                chinput = Console.ReadKey();
+                s.Push(chinput.KeyChar);
+            }
+
+            Console.WriteLine("Keys in reverse order are:");
+
+            for (int i = 0; i < numberOfKeys; i++)
+            {
+                Console.WriteLine(s.Pop());
+            }
+        }
     }
 }

10d55dbff5166352029707030c32c1e90be431c6
Ciprian <ciprian.strechie@gmail.com>
2020-03-20 15:18:34 +0200

    w3ex1

---
+++ week3/Ex1/Ex1/MyList.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex1
+{
+    class MyList<T> : ICollection, IEnumerable, IList, IStructuralComparable, IStructuralEquatable, ICloneable
+    {
+        private T[] data;
+
+        public int Count => ((ICollection)data).Count;
+
+        public bool IsSynchronized => data.IsSynchronized;
+
+        public object SyncRoot => data.SyncRoot;
+
+        public bool IsFixedSize => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public object this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public MyList(int dimension)
+        {
+            this.data = new T[dimension];
+        }
+
+        public void Add(T elem)
+        {
+            data[data.Length] = elem;
+        }
+
+        public void Remove(T value)
+        {
+            T[] tmp = new T[data.Length - 1];
+            for (var i = 0; i < data.Length; i++)
+            {
+                if (data[i].Equals(value))
+                {
+                    continue;
+                }
+                tmp[tmp.Length] = data[i];
+            }
+
+            data = tmp;
+        }
+
+        public T Find(T elem)
+        {
+            return Array.Find(this.data, (el) => el.Equals(elem));
+        }
+
+        public void Clear()
+        {
+            Array.Clear(data, 0, data.Length - 1);
+        }
+
+        public bool Contains(T value)
+        {
+            return Array.Exists(data, (el) => el.Equals(value));
+        }
+
+        public int IndexOf(T value)
+        {
+            return Array.IndexOf(data, value);
+        }
+
+        public void Insert(int index, T value)
+        {
+            if (index < 0 || index > data.Length + 1)
+            {
+                throw new ArgumentOutOfRangeException("Invalid index!");
+            }
+
+            T[] tmp = new T[data.Length + 1];
+
+            for (var i = 0; i < data.Length; i++)
+            {
+                if (i == index)
+                {
+                    tmp[tmp.Length] = value;
+                }
+                else
+                {
+                    tmp[tmp.Length] = data[i];
+                }
+               
+            }
+
+            data = tmp;
+
+
+        }
+
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= data.Length)
+            {
+                throw new ArgumentOutOfRangeException("Invalid index!");
+            }
+
+            T[] tmp = new T[data.Length - 1];
+            for (var i = 0; i < data.Length; i++)
+            {
+                if (i == index)
+                {
+                    continue;
+                }
+                tmp[tmp.Length] = data[i];
+            }
+
+            data = tmp;
+        }
+
+
+        public void CopyTo(Array array, int index)
+        {
+            data.CopyTo(array, index);
+        }
+
+
+
+        /*
+         * Implementations
+         */
+
+        public IEnumerator GetEnumerator()
+        {
+            return data.GetEnumerator();
+        }
+
+        public object Clone()
+        {
+            return data.Clone();
+        }
+
+        public bool Equals(object other, IEqualityComparer comparer)
+        {
+            return ((IStructuralEquatable)data).Equals(other, comparer);
+        }
+
+        public int GetHashCode(IEqualityComparer comparer)
+        {
+            return ((IStructuralEquatable)data).GetHashCode(comparer);
+        }
+
+        public int CompareTo(object other, IComparer comparer)
+        {
+            return ((IStructuralComparable)data).CompareTo(other, comparer);
+        }
+    }
+}
---
+++ week3/Ex1/Ex1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+/*            Console.WriteLine("Hello World!");
+                       List<int> l = new List<int>();*/
+
+
+            MyList<int> ml = new MyList<int>(5);
+            ml.Add(10);
+            ml.Add(20);
+            ml.Add(30);
+
+            int found1 = ml.Find(20);
+            Console.WriteLine("Elem found is {0}", found1);
+
+            MyList<string> ml2 = new MyList<string>(7);
+            ml2.Add("sss");
+            ml2.Add("ff");
+            ml2.Add("gggg");
+
+            string found2 = ml2.Find("gggg");
+            Console.WriteLine("Elem is ", found2);
+        }
+    }
+}

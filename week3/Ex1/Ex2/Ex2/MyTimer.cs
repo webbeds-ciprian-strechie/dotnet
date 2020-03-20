e58887b13f42c9a5c6a47b1e0c23257b529d0d1e
Ciprian <ciprian.strechie@gmail.com>
2020-03-20 16:52:46 +0200

    Ex2

---
+++ week3/Ex1/Ex2/Ex2/MyTimer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+delegate void Displayer(int n);
+
+namespace Ex2
+{
+    
+    class MyTimer
+    {
+        private static bool status = true;
+
+        public static void InitTimer(int seconds)
+        {
+            Displayer d = new Displayer(Display);
+            
+            long startTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            long current;
+
+            Console.WriteLine("Press any key to prevent exit...");
+            var tHold = Task.Run(() => Console.ReadKey(true));
+
+            while (!tHold.IsCompleted)
+            {
+                current = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+                if (current - startTime >= seconds)
+                {
+                    d(seconds);
+                    startTime = current;
+                }
+            }
+        }
+
+
+        private static void Display(int n)
+        {
+            Console.WriteLine("Elipsed {0} sec", n);
+        }
+
+    }
+}
---
+++ week3/Ex1/Ex2/Ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Get interval in seconds:");
+            int sec = int.Parse(Console.ReadLine());
+            Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+            MyTimer.InitTimer(sec);
+
+            Console.WriteLine("Shutingdown the application...");
+
+            //MyTimer.StopTimer();
+
+        }
+    }
+}

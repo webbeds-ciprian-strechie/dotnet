5d24096a2e0a828afaec75e048cd72a806d5c7ff
Ciprian <ciprian.strechie@gmail.com>
2020-03-25 00:56:52 +0200

    w3Ex4

---
+++ week3/homework/Ex4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Get a number");
+            int i = int.Parse(Console.ReadLine());
+            if (i < 1 || i > 100)
+            {
+                throw new RangeException<int>(1, 100);
+            }
+
+            Console.WriteLine("Get a date");
+            DateTime d = DateTime.Parse(Console.ReadLine());
+
+            DateTime dStart = DateTime.Parse("1.1.1980");
+            DateTime dEnd = DateTime.Parse("31.12.2013");
+            if (d < dStart || d > dEnd)
+            {
+                throw new RangeException<DateTime>(dStart, dEnd);
+            }
+        }
+    }
+}
---
+++ week3/homework/Ex4/RangeException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex4
+{
+    class RangeException<T> : Exception
+    {
+        private static readonly string DefaultMessage = "Invalid range";
+        public T RangeStart { get; set; }
+        public T RangeEnd { get; set; }
+
+        public RangeException() : base(DefaultMessage) { }
+        public RangeException(string message) : base(message) { }
+        public RangeException(string message, System.Exception innerException) : base(message, innerException) { }
+
+        public RangeException(T rangeStart, T rangeEnd) : base(DefaultMessage)
+        {
+            RangeStart = rangeStart;
+            RangeEnd = rangeEnd;
+        }
+    }
+}

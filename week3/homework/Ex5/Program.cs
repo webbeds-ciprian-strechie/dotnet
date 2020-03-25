f7b500a4333ce0f1c6335367ad49d05c68571a88
Ciprian <ciprian.strechie@gmail.com>
2020-03-25 17:09:01 +0200

    w3Ex5

---
+++ week3/homework/Ex5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1 };
+            for (var i = 0; i < 10; i++)
+            {
+                numbers.Add(ReadNumber(numbers[numbers.Count - 1], 100));
+            }
+
+            numbers.Add(100);
+
+            Console.WriteLine(String.Join<int>('<', numbers));
+        }
+
+        public static int ReadNumber(int start, int end)
+        {
+            int n;
+            Console.WriteLine("Get a number:");
+            try
+            {
+                n = int.Parse(Console.ReadLine());
+                if (n <= start || n >= end)
+                {
+                    throw new Exception($"The number need to be greater than {start} and lower than {end}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Invalid number or non-number text is entered", ex);
+            }
+
+            return n;
+        }
+    }
+}

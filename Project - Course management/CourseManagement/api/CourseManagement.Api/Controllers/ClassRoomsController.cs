31fbdf345e03f5c910d2d860148b5b6cea55b6b3
ciprian.strechie <ciprian.strechie@webbeds.com>
2020-07-22 20:46:10 +0300

    project -- added other entities

---
+++ Project - Course management/CourseManagement/api/CourseManagement.Api/Controllers/ClassRoomsController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CourseManagement.Domain.Entities;
+using CourseManagement.Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CourseManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ClassRoomsController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public ClassRoomsController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ClassRooms
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ClassRoom>>> GetClassRooms()
+        {
+            return await _context.ClassRooms.ToListAsync();
+        }
+
+        // GET: api/ClassRooms/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ClassRoom>> GetClassRoom(int id)
+        {
+            var classRoom = await _context.ClassRooms.FindAsync(id);
+
+            if (classRoom == null)
+            {
+                return NotFound();
+            }
+
+            return classRoom;
+        }
+
+        // PUT: api/ClassRooms/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClassRoom(int id, ClassRoom classRoom)
+        {
+            if (id != classRoom.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(classRoom).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClassRoomExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ClassRooms
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<ClassRoom>> PostClassRoom(ClassRoom classRoom)
+        {
+            _context.ClassRooms.Add(classRoom);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetClassRoom", new { id = classRoom.Id }, classRoom);
+        }
+
+        // DELETE: api/ClassRooms/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ClassRoom>> DeleteClassRoom(int id)
+        {
+            var classRoom = await _context.ClassRooms.FindAsync(id);
+            if (classRoom == null)
+            {
+                return NotFound();
+            }
+
+            _context.ClassRooms.Remove(classRoom);
+            await _context.SaveChangesAsync();
+
+            return classRoom;
+        }
+
+        private bool ClassRoomExists(int id)
+        {
+            return _context.ClassRooms.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Project - Course management/CourseManagement/api/CourseManagement.Api/Controllers/OfficeAssignmentsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CourseManagement.Domain.Entities;
+using CourseManagement.Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CourseManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class OfficeAssignmentsController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public OfficeAssignmentsController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/OfficeAssignments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OfficeAssignment>>> GetOfficeAssignments()
+        {
+            return await _context.OfficeAssignments.ToListAsync();
+        }
+
+        // GET: api/OfficeAssignments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OfficeAssignment>> GetOfficeAssignment(int id)
+        {
+            var officeAssignment = await _context.OfficeAssignments.FindAsync(id);
+
+            if (officeAssignment == null)
+            {
+                return NotFound();
+            }
+
+            return officeAssignment;
+        }
+
+        // PUT: api/OfficeAssignments/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOfficeAssignment(int id, OfficeAssignment officeAssignment)
+        {
+            if (id != officeAssignment.TeacherID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(officeAssignment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OfficeAssignmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/OfficeAssignments
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<OfficeAssignment>> PostOfficeAssignment(OfficeAssignment officeAssignment)
+        {
+            _context.OfficeAssignments.Add(officeAssignment);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (OfficeAssignmentExists(officeAssignment.TeacherID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetOfficeAssignment", new { id = officeAssignment.TeacherID }, officeAssignment);
+        }
+
+        // DELETE: api/OfficeAssignments/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<OfficeAssignment>> DeleteOfficeAssignment(int id)
+        {
+            var officeAssignment = await _context.OfficeAssignments.FindAsync(id);
+            if (officeAssignment == null)
+            {
+                return NotFound();
+            }
+
+            _context.OfficeAssignments.Remove(officeAssignment);
+            await _context.SaveChangesAsync();
+
+            return officeAssignment;
+        }
+
+        private bool OfficeAssignmentExists(int id)
+        {
+            return _context.OfficeAssignments.Any(e => e.TeacherID == id);
+        }
+    }
+}
